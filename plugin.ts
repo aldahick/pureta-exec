b5ab74463b8803164aa5d1182bb7cb46631194c3
Alex Hicks <alex@alexhicks.net>
2018-03-28 16:06:16 -0400

    Update for changes in Pureta

--- plugin.ts
+++ plugin.ts
@@ -3,9 +3,5 @@ import * as pureta from "pureta";
 export default class ExecPlugin extends pureta.Plugin {
     dirs = {};
 
-    async registerHandlers() {
-        this.app.on("app:start", async () => {
-            this.app.logger.info("Hello!");
-        });
-    }
+    async registerHandlers() { }
 }
